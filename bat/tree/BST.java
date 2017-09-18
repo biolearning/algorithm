02bb0b81a35ad9417781522db53dc49a10cf41f8
lwang <lwang@calix.com>
2017-09-18 23:05:31 +0800

    build tree based on inorder and postorder seq

--- bat/tree/BST.java
+++ bat/tree/BST.java
@@ -14,6 +14,35 @@ public class BST {
 		return root;		
 	}
 	
+	//build tree from inorder and postorder sequence
+	public BSTNode buildTree(int[] inorder, int[] postorder) {
+        return f(inorder, 0, inorder.length, postorder, 0, postorder.length);        
+    }
+    
+	//helper method to buildtree  from inorder and postorder sequence
+	//inorder: the inorder sequence of tree
+	//s1: start of inorder, inclusive
+	//e1: end of inorder, exclusive
+	//postorder: the postorder sequence of tree
+	//s2: end of postorder, inclusive
+	//e2: end of postorder, exclusive
+    private BSTNode f(int[] inorder, int s1, int e1, int[] postorder, int s2, int e2) {
+        if (s1 < 0 || e1 > inorder.length || s2 < 0 || e2 > postorder.length || s1 >= e1 || s2 >= e2) return null;
+        
+        BSTNode node = new BSTNode(postorder[e2-1]);
+        
+        int index = s1;
+        for(; index < e1 && inorder[index] != postorder[e2-1]; index++);
+        
+        // inorder: s1 -> index,  index + 1 -> e1   index is exlusive, before it is the root;
+        //postorder: s2 -> s2 + (index - s1) -> e2 -1, because e2 is the root, it is exclusive. 
+        //(index - s1) is the length of left subtree, e.g. [3, 3 + 3) means 3 ,4, 5 so 6 is exclusive 
+        node.left = f(inorder, s1, index, postorder, s2, s2 + (index - s1));
+        node.right = f(inorder, index + 1, e1, postorder, s2 + (index - s1) , e2-1);   
+        
+        return node;
+    }
+	
 	private void insert(BSTNode node, int value) {
 		if (value > node.value){
 			if (node.right == null) node.right = new BSTNode(value);
@@ -152,8 +181,10 @@ public class BST {
 		nums = new int[]{41, 26, 10, 1, 39, 4, 2, 7, 23, 11};
 		System.out.println(Arrays.toString(nums));
 		
-		
 		BST bst = new BST();
+		
+		bst.buildTree(new int[]{2,1}, new int[]{2,1});
+		
 		BSTNode root = bst.buildBST(nums);
 		
 		BSTNode findNode = bst.find(root, nums[4]);
